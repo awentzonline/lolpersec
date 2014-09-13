1010194e40c06cb96b58054b05c2ec0295120916
Adam Wentz <awentz@theonion.com>
2014-09-13 16:23:48 -0600

    What was the most lol-worthy thing? Isyj

--- lolpersec/lps.py
+++ lolpersec/lps.py
@@ -2,15 +2,19 @@ from gevent.monkey import patch_all; patch_all()
 
 import os
 import time
+from collections import Counter
 
 import gevent
 import gipc
 import matplotlib
 from gevent.queue import Queue
 from gtwittools.gutils import (
-    echo_queue, never_surrender, sampler, spawn_greenlets, spawn_processes)
+    echo_queue, fanout, never_surrender, sampler,
+    spawn_greenlets, spawn_processes
+)
 from gtwittools.tweetin import (
-    extract_statuses, filter_twitter, get_twitter_api)
+    extract_statuses, filter_twitter, get_twitter_api
+)
 
 
 matplotlib.use('Agg')
@@ -24,6 +28,8 @@ TWEET_SAMPLE_WINDOW = int(os.environ.get('TWEET_SAMPLE_WINDOW', 0)) or 5.0  # se
 
 counter = 0
 last_t = time.time()
+top_tweet = None
+
 
 def sample_counter():
     global counter, last_t
@@ -45,18 +51,80 @@ def count_phrases(phrase_q, phrase):
         counter += text.count(phrase)
 
 
-def sampler_process(buffer_writer, fn, phrase='lol'):
+class TopTweet(object):
+    def __init__(self):
+        self.this_counter = Counter()
+        self.last_counter = Counter()
+
+    @property
+    def top_tweet_url(self):
+        counts = self.windowed_counts
+        top = counts.most_common(1)
+        if top:
+            return top[0][0]  # strip off count
+        return None
+
+    @property
+    def windowed_counts(self):
+        counts = Counter(self.this_counter)
+        counts.update(self.last_counter)
+        return counts
+
+    def update(self, url):
+        self.this_counter[url] += 1
+
+    def advance_window(self):
+        self.last_counter = self.this_counter
+        self.this_counter = Counter()
+
+
+top_tweet = TopTweet()
+
+
+def count_top_tweet(status_q):
+    """Find the most replied to tweet."""
+    global top_tweet
+
+    for item in status_q:
+        replied_id = item.get('in_reply_to_status_id')
+        if not replied_id:
+            continue
+        user_name = item.get('in_reply_to_screen_name', 'foo')
+        url = "https://twitter.com/{}/status/{}".format(
+            user_name, replied_id
+        )
+        top_tweet.update(url)
+        if DEBUG:
+            print url
+
+
+def aggregate_sampler_data(output_writer, buffer_q):
+    for buffer_item in buffer_q:
+        top_tweet_url= top_tweet.top_tweet_url
+        top_tweet.advance_window()
+        output_writer.put((buffer_item, top_tweet_url))
+
+
+def sampler_process(output_writer, fn, phrase='lol'):
+    sample_buffer_q = Queue()
     raw_status_q = Queue()
     status_q = Queue()
+    phrase_status_q = Queue()
+    top_tweet_status_q = Queue()
     twitter_api = get_twitter_api()
     confs = [
-        [sampler, buffer_writer, fn, TWEET_SAMPLE_WINDOW, TWEET_INTERVAL],
+        [sampler, sample_buffer_q, fn, TWEET_SAMPLE_WINDOW, TWEET_INTERVAL],
+        [aggregate_sampler_data, output_writer, sample_buffer_q],
         [filter_twitter, twitter_api, status_q, [phrase]],
-        [extract_statuses, status_q, raw_status_q],
+        [fanout, status_q, [phrase_status_q, top_tweet_status_q]],
+        [extract_statuses, phrase_status_q, raw_status_q],
         [count_phrases, raw_status_q, phrase],
+        # top tweet
+        [count_top_tweet, top_tweet_status_q]
     ]
-    for conf in confs:
-        conf[0] = never_surrender(conf[0])
+    if not DEBUG:
+        for conf in confs:
+            conf[0] = never_surrender(conf[0])
     spawn_greenlets(confs)
 
 
@@ -99,12 +167,14 @@ def render_plot(plot_conf, filename='/tmp/test.png'):
 def configure_plots(buffer_reader, plot_q):
     """Transform the sample buffer into some plotting commands."""
     while True:
-        values = buffer_reader.get()
+        values, top_tweet_url = buffer_reader.get()
         caption = time.strftime('%I:%M %p %Z on %a, %x')
         if len(values):
             avg = sum(values) / len(values)
             stat_info = 'Average {0:.2f} LOL/s'.format(avg)
-            caption = '{} at {}'.format(stat_info, caption) 
+            caption = '{} at {}'.format(stat_info, caption)
+        if top_tweet_url:
+            caption += ' Top tweet: {}'.format(top_tweet_url) 
         conf = dict(
             values=values,
             caption=caption,
@@ -135,8 +205,9 @@ def renderer_process(buffer_reader, output_dir='/tmp/'):
         [plotter, plot_q, rendered_q, output_dir],
         [tweet_rendered, twitter_api, rendered_q],
     ]
-    for conf in confs:
-        conf[0] = never_surrender(conf[0])
+    if not DEBUG:
+        for conf in confs:
+            conf[0] = never_surrender(conf[0])
     spawn_greenlets(confs)
 
 
